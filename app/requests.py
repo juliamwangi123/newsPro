3ef4db4b8fc7cef849cd8d5106be08c2d5e8069f
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-04 23:34:29 +0300

    get both base url  and api key to request file

--- app/requests.py
+++ app/requests.py
@@ -1,4 +1,11 @@
 from app import app
+import urllib.request
+import json
+from app.models import Headlines
+
 
 # Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+# getting the base url
+base_url=app.config['NEWS_BASE_URL']
