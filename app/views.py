faa03e021402657f55069d5ce0f204342fa046b6
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-05 11:39:55 +0300

    render cnn articles from views

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from turtle import title
 from flask import render_template
 from app import app
-from .requests import get_headlines, get_bbc
-# Views
+from .requests import get_cnn, get_headlines, get_bbc,get_cnn
+# Viewst_
 @app.route('/')
 def index():
 
@@ -20,3 +20,8 @@ def bbc():
 
     bbcNews=get_bbc()
     return render_template('bbc.html', news=bbcNews, title='bbc')
+
+@app.route('/cnn')
+def cnn():
+    cnnNews= get_cnn()
+    return render_template('cnn.html', news=cnnNews, title='bcc') 
