7eeca8a6088f6ea5260ca9cdf1e8287e10cad399
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-05 14:10:03 +0300

    Try to commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from .config import DevConfig
+from config import DevConfig
 
 app=Flask(__name__,instance_relative_config = True)
 # Setting up configuration
@@ -9,4 +9,3 @@ app.config.from_pyfile('config.py')
 from .requests import configure_request
 configure_request(app)
 
-from app import views
