715481363313781639632cdffba729b8ef210b6c
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-05 14:28:17 +0300

    fix bug

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from config import DevConfig
+from .config import DevConfig
 
 app=Flask(__name__,instance_relative_config = True)
 # Setting up configuration
 app.config.from_object(DevConfig)
 app.config.from_pyfile('config.py')
 
+
 from .requests import configure_request
 configure_request(app)
-
+from app import views
