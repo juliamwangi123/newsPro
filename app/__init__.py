6113ba11204eaccbe20215e34e7f29cc6fb38592
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-05-06 15:03:23 +0300

    deployment

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from .config import DevConfig
 app=Flask(__name__,instance_relative_config = True)
 # Setting up configuration
 app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+# app.config.from_pyfile('config.py')
 
 
 from .requests import configure_request
